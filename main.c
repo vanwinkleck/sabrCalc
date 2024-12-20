18bdb80037d548269298301c869f8e8620014d2d
vanwinkleck <vanwinkleck@protonmail.com>
2024-12-19 19:45:35 -0600

    initial commit, started calculator

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	char stat[] = "";
+	char i = 0;
+
+	while (1) 
+	{
+		printf("\nselect which sabr metric to calculate\n");
+		printf("available choices are: \n");
+		printf("exit by typing 'done'\n");
+
+		exit(0);
+	}
+
+	return 0;
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+Sabr metrics calculator implemented in C
