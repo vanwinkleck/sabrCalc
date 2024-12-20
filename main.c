97872d1f82d61a3fd027b020291e159c9c8d8fc0
vanwinkleck <vanwinkleck@protonmail.com>
2024-12-19 20:09:38 -0600

    expanded out, still not close to done

--- main.c
+++ main.c
@@ -4,14 +4,34 @@
 
 int main()
 {
-	char stat[] = "";
+	char stat[256];
 	char i = 0;
+	float operand1, operand2, out;
 
 	while (1) 
 	{
 		printf("\nselect which sabr metric to calculate\n");
 		printf("available choices are: \n");
 		printf("exit by typing 'done'\n");
+
+		fgets(stat, sizeof(stat), stdin);
+		printf("You input: %s\n", stat);
+		printf("calculating...");
+
+		while ((getchar()) != '\n');
+		switch (char) 
+		{
+			case 'h9':
+				printf("Input a pitcher's hits allowed\n");
+				scanf("%f", &operand1);
+		  		printf("Input the same pitcher's innings pitched");
+				scanf("%f", &operand2);
+				out = (operand1 / operand2) * 9;
+				printf("This pitcher's H/9 is: %f\n", out);
+				break;
+		}
+
+		break;
 
 		exit(0);
 	}
