78faf84e63b547db62897ca9497de029dc7d84e9
vanwinkleck <vanwinkleck@protonmail.com>
2025-02-05 21:47:20 -0600

    added era+, havent got it working right yet

--- main.c
+++ main.c
@@ -13,9 +13,16 @@ int calculateOPSPlus (float leagueOBP, float OBP, float leagueSLG, float SLG) {
 	return 100 * ((OBP / leagueOBP) + (SLG / leagueSLG) - 1);
 }
 
+// This isn't working correctly
+int calculateERAPlus(int parkFactor, float leagueERA, float ERA) {
+	return 100 * leagueERA / ERA * parkFactor;
+}
+
+
+
 int main()
 {
-	float ip, hr;
+	/* float ip, hr;
 
 	printf("Enter innings pitched: ");
 	scanf("%f", &ip);
@@ -28,6 +35,9 @@ int main()
 	printf("This pitcher's home runs per 9 is %.2f:\n", hr9);
 
 	int opsPlus = calculateOPSPlus(.315, .394, .399, .379);
-	printf ("Brendan Donvan's OPS plus is: %i:\n", opsPlus);
+	printf ("Brendan Donvan's OPS plus is: %i:\n", opsPlus); */ 
+
+	float eraPlus = calculateERAPlus(100, 4.02, 3.51);
+	printf ("Sonny Gray's ERA+ is: %.2f:\n", eraPlus);
 	return 0;
 }
