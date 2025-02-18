01d164e9940a02304738a5d757e3e03e3d1a9e98
vanwinkleck <vanwinkleck@protonmail.com>
2025-02-18 00:09:13 -0600

    switched to structs in pitching calculations, added todo list

--- pitchingCalculations.c
+++ pitchingCalculations.c
@@ -1,21 +1,41 @@
 #include <stdio.h>
 
-float calculatehr9(float ip, float hr) {
-	return (hr / ip) * 9;
-}
+ struct leagueNumbers {
+  float leagueERA;
+  double leagueHRAllowed;
+  double leagueBBAllowed;
+  double leagueHBPAllowed;
+  double leagueK;
+  double leagueIP;
+};
 
-float calculateh9(float hits, float ip) {
-	return (hits / ip ) * 9;
-}
+struct pitcherNumbers {
+  double HRAllowed;
+  double BBAllowed;
+  double HPBAllowed;
+  double HitsAllowed;
+  double K;
+  double IP;
+};
 
-float calculateFIPConstant(float ERA, double HR, double BB, double HBP, double K, double IP)
+
+float calculateHR9(struct pitcherNumbers pitcher)
 {
-  return ERA - (((HR * 13) + (3*(BB+HBP)) - (2 * K)) / IP);
+  return ( pitcher.HRAllowed / pitcher.IP ) * 9;
 }
 
-float calculateFIP(double HR, double BB, double HBP, double K, double IP, double fipConstant)
+float calculateH9(struct pitcherNumbers pitcher) {
+	return ( pitcher.HitsAllowed / pitcher.IP ) * 9;
+}
+
+float calculateFIPConstant(struct leagueNumbers year)
 {
-  return ((HR * 13) + (3 * (BB + HBP)) - (2 * K)) / IP + fipConstant;
+  return year.leagueERA - (((year.leagueHRAllowed * 13) + (3 * (year.leagueBBAllowed + year.leagueHBPAllowed)) - (2 * year.leagueK)) / year.leagueIP); 
+}
+
+float calculateFIP(struct pitcherNumbers pitcher, float fipConstant)
+{
+  return ((pitcher.HRAllowed * 13) + (3 * (pitcher.BBAllowed + pitcher.HPBAllowed)) - (2 * pitcher.K)) / pitcher.IP + fipConstant;
 }
 
 
@@ -27,11 +47,33 @@ int calculateERAPlus(float parkFactor, float leagueERA, float ERA) {
 
 int main() {
 
+  struct leagueNumbers s2024;
+  struct pitcherNumbers gray;
 
-  float sonnyFIP;
+  s2024.leagueERA = 4.07;
+  s2024.leagueHRAllowed = 5453;
+  s2024.leagueBBAllowed = 14929;
+  s2024.leagueHBPAllowed = 2020;
+  s2024.leagueK = 41197;
+  s2024.leagueIP = 43116.1;
 
-  sonnyFIP = calculateFIP(21, 39, 3, 203, 166.1, calculateFIPConstant(4.07, 5453, 14929, 2020, 41197, 43116.1));
+  gray.HRAllowed = 21;
+  gray.BBAllowed = 39;
+  gray.HPBAllowed = 3;
+  gray.HitsAllowed = 142;
+  gray.K = 203;
+  gray.IP = 166.1;
+
+  float sonnyFIP, sonnyH9, sonnyHR9;
+
+  sonnyFIP = calculateFIP(gray, calculateFIPConstant(s2024));
   printf("Sonny Gray's 2024 FIP is: %.2f\n", sonnyFIP);
+
+  sonnyH9 = calculateH9(gray);
+  printf("Sonny Gray's 2024 H/9 is : %.1f\n", sonnyH9);
+
+  sonnyHR9 = calculateHR9(gray);
+  printf("Sonny Gray's 2024 HR/9 is: %.1f\n", sonnyHR9);
 
   return 0;
 }
