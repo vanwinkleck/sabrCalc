432601a9c69a55245f2fb571715fff1da5ebb921
vanwinkleck <vanwinkleck@protonmail.com>
2025-02-18 17:27:51 -0600

    switched to structs in batting calculations, still need to find the formula to adjust for ballpark in OPS+

--- battingCalculations.c
+++ battingCalculations.c
@@ -1,41 +1,81 @@
 #include <stdio.h>
 
-float calculateOnBase(double hits, double walks, double HBP, double atBats, double sacFly) {
-	return (hits + walks + HBP) / (atBats + walks + HBP + sacFly); 
-}
-
-double calculateTotalBases(int singles, int doubles, int triples, int homeRuns) {
-	return singles + (doubles * 2) + (triples * 3) + (homeRuns * 4);
+struct leagueNumbers{
+  double leagueOBP;
+  double leagueSLG;
 };
 
-float calculateSlug(int singles, int doubles, int triples, int HR, int atBats) {
-	return calculateTotalBases(singles, doubles, triples, HR) / atBats;
+struct playerNumbers {
+  double hits;
+  double BB;
+  double HBP;
+  double atBats;
+  double SF;
+  double SO;
+  double singles;
+  double doubles;
+  double triples;
+  double HR;
+};
+
+
+float calculateOnBase(struct playerNumbers player) {
+	return (player.hits + player.BB + player.HBP) / (player.atBats + player.BB + player.HBP + player.SF); 
 }
 
-float calculateOPS(double singles, double doubles, double triples, double HR, double hits, double walks, double HBP, double atBats, double sacFly) {
-	return calculateSlug(singles, doubles, triples, HR, atBats) + calculateOnBase(hits, walks, HBP, atBats, sacFly);
+double calculateTotalBases(struct playerNumbers player) {
+	return player.singles + (player.doubles * 2) + (player.triples * 3) + (player.HR * 4);
+};
+
+float calculateSlug(struct playerNumbers player) {
+	return calculateTotalBases(player) / player.atBats;
+}
+
+float calculateOPS(struct playerNumbers player) {
+	return calculateSlug(player) + calculateOnBase(player);
 }
 // Need to adjust for ballpark. 
-int calculateOPSPlus (float leagueOBP, float OBP, float leagueSLG, float SLG) {
-	return 100 * ((OBP / leagueOBP) + (SLG / leagueSLG) - 1);
+int calculateOPSPlus (struct playerNumbers player, struct leagueNumbers lg) {
+	return 100 * (calculateOnBase(player) / lg.leagueOBP + calculateSlug(player) / lg.leagueSLG - 1);
 }
 
-float calculateBABIP(double hits, double HR, double atBats, double SO, double SF)
+float calculateBABIP(struct playerNumbers player)
 {
-  return (hits - HR) / (atBats - SO - HR + SF);
+  return (player.hits - player.HR) / (player.atBats - player.SO - player.HR + player.SF);
 }
 
-int test()
+int main()
 {
-	
-  float onBase = calculateOnBase(163, 47, 13, 587, 5);
+
+  struct playerNumbers donny;
+  struct leagueNumbers year;
+  int parkFactor = 98;
+  
+  donny.hits = 163;
+  donny.BB = 47;
+  donny.HBP = 13;
+  donny.atBats = 587;
+  donny.SF = 5;
+  donny.SO = 81;
+  donny.singles = 112;
+  donny.doubles = 34;
+  donny.triples = 3;
+  donny.HR = 14;
+
+  year.leagueOBP = 0.312;
+  year.leagueSLG = 0.399;
+
+  float onBase = calculateOnBase(donny);
 	printf ("Brendan Donovan's onBase is: %.3f:\n", onBase);
 
-	float slug = calculateSlug(112, 34, 3, 14, 587);
+	float slug = calculateSlug(donny);
 	printf ("Brendan Donovan's slugging is: %.3f\n", slug);
 
-	float ops = calculateOPS(112, 34, 3, 14, 163, 47, 13, 587, 5);
+	float ops = calculateOPS(donny);
 	printf ("Brendan Donovan's ops is: %.3f\n", ops);
+
+  int opsPlus = calculateOPSPlus(donny, year);
+  printf ("Brendan Donovan's 2024 OPS+ is: %i\n", opsPlus);
 
 	return 0;
 }
